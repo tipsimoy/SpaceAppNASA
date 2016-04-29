cce7757325359618b8324da696f5ebc9dc790c3d
tipsimoy <tip.simoy@gmail.com>
2016-04-29 19:20:29 +0800

    Added files via upload

---
+++ Arduino/motor_control.ino
@@ -0,0 +1,59 @@
+int leftForward = 3;
+int leftBackward = 4;
+int rightForward = 6;
+int rightBackward = 7;
+int enablePin = 5;
+char state;
+int flag = 0;
+ 
+void setup() {
+
+    pinMode(leftForward, OUTPUT);
+    pinMode(leftBackward, OUTPUT);
+    pinMode(rightForward, OUTPUT);
+    pinMode(rightBackward, OUTPUT);
+    pinMode(enablePin, OUTPUT);
+
+    digitalWrite(enablePin, HIGH);
+
+    Serial.begin(9600);
+}
+ 
+void loop() 
+{
+
+    if(Serial.available() > 0)
+    {     
+      state = Serial.read();   
+      flag=0;
+    }  
+    
+    if (state == '1') 
+    {
+        digitalWrite(leftForward, HIGH);
+        digitalWrite(leftBackward, LOW);
+        digitalWrite(rightForward, LOW);
+        digitalWrite(rightBackward, HIGH);
+    }
+    else if (state == '2') 
+    {
+        digitalWrite(leftForward, LOW);
+        digitalWrite(leftBackward, HIGH);
+        digitalWrite(rightForward, HIGH);
+        digitalWrite(rightBackward, LOW);
+    }
+    else if (state == '3')
+    {
+        digitalWrite(leftForward, HIGH);
+        digitalWrite(leftBackward, LOW);
+        digitalWrite(rightForward, HIGH);
+        digitalWrite(rightBackward, LOW);
+    }
+    else if (state == '4') 
+    {
+        digitalWrite(leftForward, LOW);
+        digitalWrite(leftBackward, HIGH);
+        digitalWrite(rightForward, LOW);
+        digitalWrite(rightBackward, HIGH);
+    }
+ }
---
+++ Arduino/sensors.ino
@@ -0,0 +1,76 @@
+#include <dht.h>
+#define dht_apin A0
+
+dht DHT;
+
+ int valueMQ5 = A2;
+ int valueMQ2 = A3;
+ int measurePin = A1;
+ int ledPower = 2; //digital 2
+ int samplingTime = 280;
+ int deltaTime = 40;
+ int sleepTime = 9680;
+ float voMeasured = 0;
+ float calcVoltage = 0;
+ float dustDensity = 0;
+ String humidity;
+ String temperature;
+ String dust;
+ String MQ2;
+ String MQ5;
+ String servernumber = "+639753349196";
+ String compiledData;
+
+ void setup()
+ {
+  Serial.begin(9600);
+  delay(1000);
+  pinMode(ledPower,OUTPUT);
+  pinMode(valueMQ2, INPUT);
+  pinMode(valueMQ5, INPUT);
+ }
+
+void loop()
+{
+ MQ2 = String(analogRead(valueMQ2));
+ MQ5 = String(analogRead(valueMQ5));
+ DHT11();
+ DUSTSENSOR();
+ temperature = String(DHT.temperature); //in degrees Celcius
+ humidity = String(DHT.humidity); //in percent
+ dust = String (dustDensity); //in mg/m3
+ compiledData = String(temperature + ' ' + humidity + ' ' + MQ5 + ' ' + MQ2 + ' ' + dust + ' ' + "JOMA143");
+ sendSMS(compiledData);
+}
+void DHT11()
+{
+  DHT.read11(dht_apin);  
+  delay(5000);
+}
+void DUSTSENSOR()
+{
+  digitalWrite(ledPower,LOW);
+  delayMicroseconds(samplingTime);
+  voMeasured = analogRead(measurePin);
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower,HIGH);
+  delayMicroseconds(sleepTime);
+  calcVoltage = voMeasured * (5.0 / 1024.0);
+  dustDensity = 0.17 * calcVoltage - 0.1;
+  delay(1000);  
+}
+
+void sendSMS(String msg)
+{
+ Serial.begin(9600); 
+ delay(2000);
+ Serial.print("AT+CMG­F=1\r");
+ delay(2000);
+ Serial.println("AT + CMGS = \"" + servernumber +"\"");
+ delay(2000);
+ Serial.print(msg);
+ delay(2000);
+ Serial.write((char)26);
+ delay(2000);
+ }
+
